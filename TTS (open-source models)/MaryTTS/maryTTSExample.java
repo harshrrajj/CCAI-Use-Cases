2112ee63e0ceeceff3d0973ff7e8903ff195fea5
harshrrajj <108732625+harshrrajj@users.noreply.github.com>
2022-07-06 13:59:31 +0530

    Update maryTTSExample.java

--- TTS (open-source models)/MaryTTS/maryTTSExample.java
+++ TTS (open-source models)/MaryTTS/maryTTSExample.java
@@ -20,7 +20,7 @@ public class maryTTSExample {
         var start = System.currentTimeMillis();
         MaryInterface marytts = new LocalMaryInterface();
         AudioInputStream audio = marytts.generateAudio("The 1881 world tour of King Kalākaua made him the first monarch to circumnavigate the globe. His agenda was to negotiate contract labor for the Kingdom of Hawaiis sugar plantations, with hopes of saving the dwindling Native Hawaiian population by drawing immigration from Asia-Pacific nations. Rumors circulated that the King secretly intended to use the trip to sell the Hawaiian Islands. He visited American legislators, had an audience with Pope Leo XIII in Rome, and met with European and Asian heads of state. In between negotiations, Kalākaua and his companions visited tourist sites and attended local Masonic lodge meetings. As a result of his visit with Thomas Edison, Iolani Palace became the first building in Hawaii with electric lighting.");
-        MaryAudioUtils.writeWavFile(MaryAudioUtils.getSamplesAsDoubleArray(audio), "R:\\Extracted\\audioOutput2.wav", audio.getFormat());  //replace the file path according to your file/location.
+        MaryAudioUtils.writeWavFile(MaryAudioUtils.getSamplesAsDoubleArray(audio), "maryTTS_result.wav", audio.getFormat());  //replace "maryTTS_result.wav" with the file path where you want to store the generated audio on your PC.
         var end = System.currentTimeMillis();
         float timeConsumed = (float)(end-start) / (float)1000;
         long afterUsedMem=Runtime.getRuntime().totalMemory()-Runtime.getRuntime().freeMemory();
